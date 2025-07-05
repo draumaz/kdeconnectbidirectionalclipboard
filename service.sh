3b3cd20b182ac6e8b0dbf756747a8e5fcc374f0b
Emma <77967471+draumaz@users.noreply.github.com>
2025-07-05 04:02:03 -0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,10 @@
 #!/system/bin/sh
-MODDIR=${0%/*}
-MNAME=$(basename $MODDIR)
 
-sleep 20
-/system/bin/pm grant org.kde.kdeconnect_tp android.permission.READ_LOGS;
-/system/bin/appops set org.kde.kdeconnect_tp SYSTEM_ALERT_WINDOW allow;
-/system/bin/am force-stop org.kde.kdeconnect_tp;
+MODDIR="${0%/*}"
+MNAME="$(basename ${MODDIR})"
+XPKG="org.kde.kdeconnect_tp"
+
+/system/bin/pm grant "${XPKG}" "android.permission.READ_LOGS"
+/system/bin/appops set "${XPKG}" "SYSTEM_ALERT_WINDOW" "allow"
+
+/system/bin/am force-stop "${XPKG}"
